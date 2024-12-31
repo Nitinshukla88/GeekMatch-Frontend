0b3368276692e31c1c6a9b64c5dbce45c43a856c
nitinshukla <nitin.shukla12004@gmail.com>
2024-12-31 09:12:47 +0530

    implemented the edit-Profile feature

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import UserCard from "./UserCard";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/appStoreSlices/userSlice";
 
 const EditProfile = ({ userInfo }) => {
   const [firstName, setFirstName] = useState(userInfo?.firstName);
@@ -8,110 +12,125 @@ const EditProfile = ({ userInfo }) => {
   const [age, setAge] = useState(userInfo?.age);
   const [about, setAbout] = useState(userInfo?.about);
   const [photo, setPhoto] = useState(userInfo?.photo || "");
+  const [error, setError] = useState("");
+  
+
+  const dispatch = useDispatch();
+
+  const handleSave = async() => {
+    setError("");
+    try {
+      const res = await axios.patch(BASE_URL + "/profile/edit" , { firstName, lastName, gender, age, about, photo }, {withCredentials : true});
+      dispatch(addUser(res?.data?.data));
+    }catch(err){
+      setError(err?.response?.data);
+    }
+  }
 
   return (
-    <div className="flex justify-center items-center">
-    <div className="flex justify-center items-center mx-12">
-      <div className="card bg-primary text-primary-content w-[35rem] my-16">
-        <div className="card-body">
-          <h2 className="card-title justify-center text-3xl">Edit Profile</h2>
-          <div className="flex">
-            <div className="mx-4">
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text text-black font-semibold">
-                First Name:
-              </span>
+    <div className="flex justify-center">
+      <div className="flex justify-center items-center mx-12">
+        <div className="card bg-primary text-primary-content w-[35rem]">
+          <div className="card-body">
+            <h2 className="card-title justify-center text-3xl">Edit Profile</h2>
+            <div className="flex">
+              <div className="mx-4">
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text text-black font-semibold">
+                      First Name:
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    className="input input-bordered w-full max-w-xs text-white"
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text text-black font-semibold">
+                      Last Name:
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    className="input input-bordered w-full max-w-xs text-white"
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text text-black font-semibold">
+                      Gender:
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                    className="input input-bordered w-full max-w-xs text-white"
+                  />
+                </label>
+              </div>
+              <div className="mx-4">
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text text-black font-semibold">
+                      Age:
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                    className="input input-bordered w-full max-w-xs text-white"
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text text-black font-semibold">
+                      About:
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    value={about}
+                    onChange={(e) => setAbout(e.target.value)}
+                    className="input input-bordered w-full max-w-xs text-white"
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs">
+                  <div className="label">
+                    <span className="label-text text-black font-semibold">
+                      Photo URL:
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Type here"
+                    value={photo}
+                    onChange={(e) => setPhoto(e.target.value)}
+                    className="input input-bordered w-full max-w-xs text-white"
+                  />
+                </label>
+              </div>
             </div>
-            <input
-              type="text"
-              placeholder="Type here"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              className="input input-bordered w-full max-w-xs text-white"
-            />
-          </label>
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text text-black font-semibold">
-                Last Name:
-              </span>
+            <p className="text-red-600 font-semibold">{error}</p>
+            <div className="card-actions justify-center mt-3">
+              <button className="btn btn-secondary" onClick={handleSave}>Save</button>
             </div>
-            <input
-              type="text"
-              placeholder="Type here"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              className="input input-bordered w-full max-w-xs text-white"
-            />
-          </label>
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text text-black font-semibold">
-                Gender:
-              </span>
-            </div>
-            <input
-              type="text"
-              placeholder="Type here"
-              value={gender}
-              onChange={(e) => setGender(e.target.value)}
-              className="input input-bordered w-full max-w-xs text-white"
-            />
-          </label>
-          </div>
-          <div className="mx-4">
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text text-black font-semibold">
-                Age:
-              </span>
-            </div>
-            <input
-              type="text"
-              placeholder="Type here"
-              value={age}
-              onChange={(e) => setAge(e.target.value)}
-              className="input input-bordered w-full max-w-xs text-white"
-            />
-          </label>
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text text-black font-semibold">
-                About:
-              </span>
-            </div>
-            <input
-              type="text"
-              placeholder="Type here"
-              value={about}
-              onChange={(e) => setAbout(e.target.value)}
-              className="input input-bordered w-full max-w-xs text-white"
-            />
-          </label>
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text text-black font-semibold">
-                Photo URL:
-              </span>
-            </div>
-            <input
-              type="text"
-              placeholder="Type here"
-              value={photo}
-              onChange={(e) => setPhoto(e.target.value)}
-              className="input input-bordered w-full max-w-xs text-white"
-            />
-          </label>
-          </div>
-          </div>
-          <div className="card-actions justify-center mt-3">
-            <button className="btn btn-secondary">Save</button>
           </div>
         </div>
       </div>
-    </div>
-    <UserCard user={ {firstName, lastName, gender, age, photo, about} }/>
+      <UserCard user={{ firstName, lastName, gender, age, photo, about }} />
     </div>
   );
 };
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const UserCard = ({ user }) => {
 
   const { firstName, lastName, about, age, gender, photo } = user;
-  console.log(about);
 
   return (
     <div className="flex justify-center my-16">
@@ -16,7 +15,7 @@ const UserCard = ({ user }) => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
-        <h3>{age + " " + gender}</h3>
+        <h3>{age + ", " + gender}</h3>
         <p>{about}</p>
         <div className="card-actions justify-center">
           <button className="btn btn-primary mx-2">Ignore</button>
