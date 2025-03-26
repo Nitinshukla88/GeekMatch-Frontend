6045ba05d53b807b164b53efe2b894e66d717afc
nitinshukla <nitin.shukla12004@gmail.com>
2025-03-26 23:05:54 +0530

    removed .env and hardcoded the url for testing

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,1 @@
-console.log("BASE_URL:", process.env.REACT_APP_BASE_URL);
-export const BASE_URL = "http://localhost:4000" || process.env.REACT_APP_BASE_URL;
+export const BASE_URL = "https://geekmatch-backend.onrender.com"
