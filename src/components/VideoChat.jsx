1fa6fe3637d2c7b2cd0d35bf5fcd2b3d58c66688
nitinshukla <nitin.shukla12004@gmail.com>
2025-05-30 23:19:11 +0530

    added the necessary changes

--- src/components/VideoChat.jsx
+++ src/components/VideoChat.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { createSocketConnection } from '../utils/socket';
-import { createAnswer, createOffer, peer, sendStream, setRemoteAnswer } from '../utils/videoChatUtils';
-import ReactPlayer from 'react-player';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { createSocketConnection } from "../utils/socket";
+import {
+  createAnswer,
+  createOffer,
+  peer,
+  sendStream,
+  setRemoteAnswer,
+} from "../utils/videoChatUtils";
+import ReactPlayer from "react-player";
 
 const VideoChat = () => {
   const { targetUserId } = useParams();
@@ -11,58 +17,85 @@ const VideoChat = () => {
   const { _id, firstName } = loggedInUser || {};
   const [myStream, setMyStream] = useState(null);
   const [remoteStream, setRemoteStream] = useState(null);
-  const[show, setShow] = useState(false);
+  const [show, setShow] = useState({ firstScreen: false, SecondScreen: false });
+  const navigate = useNavigate();
 
-  const getUserMedia = async()=>{
-    const stream = await navigator.mediaDevices.getUserMedia({audio : false, video : true});
+  const getUserMedia = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    });
     setMyStream(stream);
-  }
+  };
 
-  const handleTrackEvent = (ev)=>{
+  const handleTrackEvent = (ev) => {
     const streams = ev.streams;
     setRemoteStream(streams[0]);
-  }
-
-  
-
-  useEffect(()=>{
-    peer.addEventListener("track", handleTrackEvent);
-    return (()=>{
-      peer.removeEventListener("track", handleTrackEvent);})
-},[peer, handleTrackEvent])
+  };
 
   useEffect(() => {
-    
+    peer.addEventListener("track", handleTrackEvent);
+    return () => {
+      peer.removeEventListener("track", handleTrackEvent);
+    };
+  }, [peer, handleTrackEvent]);
+
+  useEffect(() => {
     if (!_id) return;
 
     const socket = createSocketConnection();
 
-    const handleNegotiation = async()=>{
+    const handleNegotiation = async () => {
       console.log("negotiation needed");
       const localOffer = await createOffer();
-      socket.emit("call-user", {firstName, _id, targetUserId, offer : localOffer});
-    }
+      socket.emit("call-user", {
+        firstName,
+        _id,
+        targetUserId,
+        offer: localOffer,
+      });
+    };
 
     peer.addEventListener("negotiationneeded", handleNegotiation);
 
     socket.emit("join-room", { firstName, _id, targetUserId });
 
-    socket.on("joined-room", async ({ firstName: newUser, _id: newUserId, targetUserId }) => {
-        console.log(`User ${newUser} joined with userId ${newUserId}, user1 id is ${targetUserId}. Sending offer...`);
+    socket.on(
+      "joined-room",
+      async ({ firstName: newUser, _id: newUserId, targetUserId }) => {
+        console.log(
+          `User ${newUser} joined with userId ${newUserId}, user1 id is ${targetUserId}. Sending offer...`
+        );
         const offer = await createOffer();
-        socket.emit("call-user", { firstName, _id, targetUserId: newUserId, offer });
-    });
+        socket.emit("call-user", {
+          firstName,
+          _id,
+          targetUserId: newUserId,
+          offer,
+        });
+      }
+    );
 
-    socket.on("incoming-call", async({ firstName: callerName, offer, _id, targetUserId }) => {
-      console.log(`Incoming call from ${callerName} with id ${_id}`, offer);
-      const answer = await createAnswer(offer);
-      socket.emit("call-accepted", {firstName, _id, targetUserId, answer});
-    });
+    socket.on(
+      "incoming-call",
+      async ({ firstName: callerName, offer, _id, targetUserId }) => {
+        console.log(`Incoming call from ${callerName} with id ${_id}`, offer);
+        const answer = await createAnswer(offer);
+        socket.emit("call-accepted", { firstName, _id, targetUserId, answer });
+      }
+    );
 
-    socket.on("call-accepted", async({firstName, _id, targetUserId, answer})=>{
-      console.log(`call got accepted`, answer);
-      await setRemoteAnswer(answer);
-    })
+    socket.on(
+      "call-accepted",
+      async ({ firstName, _id, targetUserId, answer }) => {
+        console.log(`call got accepted`, answer);
+        await setRemoteAnswer(answer);
+      }
+    );
+
+    socket.on("partner-disconnected", async ({ message, roomId }) => {
+      setShow({ firstScreen: true, SecondScreen: false });
+    });
 
     return () => {
       socket.disconnect();
@@ -70,22 +103,41 @@ const VideoChat = () => {
     };
   }, [_id, targetUserId, firstName]);
 
-  useEffect(()=>{
+  useEffect(() => {
     getUserMedia();
-  },[getUserMedia])
+  }, [getUserMedia]);
 
-  return <div className='flex'>
-    <button className='btn btn-primary' onClick={(e)=> {
-      console.log("Sending mySteam...");
-      sendStream(myStream);
-      setShow(true);
-    }
-      }>Connect call</button>
-      <div className='flex'>
-    {show && <>
-    <ReactPlayer url={myStream} playing muted/>
-    <ReactPlayer url={remoteStream} playing muted/></>}</div>
-  </div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex">
+        <div>
+          {show?.firstScreen && <ReactPlayer url={myStream} playing muted />}
+        </div>
+        <div>
+          {show?.SecondScreen && (
+            <ReactPlayer url={remoteStream} playing muted />
+          )}
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <button
+          className="btn btn-primary"
+          onClick={(e) => {
+            setShow({ firstScreen: true, SecondScreen: true });
+            sendStream(myStream);
+          }}
+        >
+          Connect call
+        </button>
+        <button
+          className="btn btn-secondary"
+          onClick={(e) => navigate("/connections")}
+        >
+          Disconnect call
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default VideoChat;
